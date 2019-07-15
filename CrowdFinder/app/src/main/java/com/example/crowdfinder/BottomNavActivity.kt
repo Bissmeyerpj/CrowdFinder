3546c60cc1ef84b0c48859342111721f0ffb3d33
Philip Bissmeyer <bissmeyerpj@gmail.com>
2019-07-15 16:35:46 -0400

    Basic Login screen and login button goes to new activity

---
+++ CrowdFinder/app/src/main/java/com/example/crowdfinder/BottomNavActivity.kt
@@ -0,0 +1,21 @@
+package com.example.crowdfinder
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_bottom_nav.*
+
+class BottomNavActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_nav)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
--- CrowdFinder/app/src/main/java/com/example/crowdfinder/MainActivity.kt
+++ CrowdFinder/app/src/main/java/com/example/crowdfinder/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.crowdfinder
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login_button.setOnClickListener {
+            val bottomNavIntent = Intent(this, BottomNavActivity::class.java)
+            startActivity(bottomNavIntent)
+        }
     }
 }
