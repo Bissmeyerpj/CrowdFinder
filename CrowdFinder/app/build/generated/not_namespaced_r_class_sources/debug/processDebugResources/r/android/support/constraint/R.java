a24e666162f8231669487ba44a30fccc4ab39689
Philip Bissmeyer <bissmeyerpj@gmail.com>
2019-07-15 17:01:36 -0400

    added bottomNav

--- CrowdFinder/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ CrowdFinder/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080022;
-        public static final int end = 0x7f08003f;
-        public static final int gone = 0x7f08004d;
-        public static final int invisible = 0x7f080057;
-        public static final int left = 0x7f08005c;
-        public static final int packed = 0x7f080073;
-        public static final int parent = 0x7f080075;
-        public static final int percent = 0x7f080079;
-        public static final int right = 0x7f08007e;
-        public static final int spread = 0x7f0800a2;
-        public static final int spread_inside = 0x7f0800a3;
-        public static final int start = 0x7f0800a8;
-        public static final int top = 0x7f0800bd;
-        public static final int wrap = 0x7f0800cc;
+        public static final int end = 0x7f080040;
+        public static final int gone = 0x7f080050;
+        public static final int invisible = 0x7f08005a;
+        public static final int left = 0x7f08005f;
+        public static final int packed = 0x7f080077;
+        public static final int parent = 0x7f080079;
+        public static final int percent = 0x7f08007d;
+        public static final int right = 0x7f080083;
+        public static final int spread = 0x7f0800a8;
+        public static final int spread_inside = 0x7f0800a9;
+        public static final int start = 0x7f0800ae;
+        public static final int top = 0x7f0800c3;
+        public static final int wrap = 0x7f0800d2;
     }
     public static final class styleable {
         private styleable() {}
--- CrowdFinder/app/src/main/java/com/example/crowdfinder/BottomNavActivity.kt
+++ CrowdFinder/app/src/main/java/com/example/crowdfinder/BottomNavActivity.kt
@@ -1,12 +1,41 @@
 package com.example.crowdfinder
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity;
 
 import kotlinx.android.synthetic.main.activity_bottom_nav.*
 
 class BottomNavActivity : AppCompatActivity() {
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        var switchTo: Fragment? = null
+        when (item.itemId) {
+            R.id.settings -> {
+//                switchTo = SettingsFragment()
+            }
+            R.id.compass -> {
+
+            }
+            R.id.friends -> {
+
+            }
+            R.id.requests -> {
+
+            }
+        }
+        switchTo?.let {
+            val ft = supportFragmentManager.beginTransaction()
+            ft.replace(R.id.fragment_container, switchTo, "MY_FRAGMENT")
+            while(supportFragmentManager.backStackEntryCount > 0) {
+                supportFragmentManager.popBackStackImmediate()
+            }
+            ft.commit()
+        }
+        true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
