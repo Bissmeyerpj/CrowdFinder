7258a6d00064a67c803dd79765d540a19575d7ec
Philip Bissmeyer <bissmeyerpj@gmail.com>
2019-07-21 22:33:49 -0400

    Last push didnt add layout files, this should fix it

--- CrowdFinder/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ CrowdFinder/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -75,18 +75,18 @@ public final class R {
 
         public static final int bottom = 0x7f080023;
         public static final int end = 0x7f080046;
-        public static final int gone = 0x7f080057;
-        public static final int invisible = 0x7f080061;
-        public static final int left = 0x7f080066;
-        public static final int packed = 0x7f080088;
-        public static final int parent = 0x7f08008a;
-        public static final int percent = 0x7f08008e;
-        public static final int right = 0x7f080094;
-        public static final int spread = 0x7f0800b9;
-        public static final int spread_inside = 0x7f0800ba;
-        public static final int start = 0x7f0800bf;
-        public static final int top = 0x7f0800d5;
-        public static final int wrap = 0x7f0800e4;
+        public static final int gone = 0x7f080058;
+        public static final int invisible = 0x7f080062;
+        public static final int left = 0x7f080067;
+        public static final int packed = 0x7f080089;
+        public static final int parent = 0x7f08008b;
+        public static final int percent = 0x7f08008f;
+        public static final int right = 0x7f080096;
+        public static final int spread = 0x7f0800bb;
+        public static final int spread_inside = 0x7f0800bc;
+        public static final int start = 0x7f0800c1;
+        public static final int top = 0x7f0800d7;
+        public static final int wrap = 0x7f0800e6;
     }
     public static final class styleable {
         private styleable() {}
--- CrowdFinder/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/swiperefreshlayout/R.java
+++ CrowdFinder/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/swiperefreshlayout/R.java
@@ -87,25 +87,25 @@ public final class R {
         public static final int blocking = 0x7f080022;
         public static final int chronometer = 0x7f08002d;
         public static final int forever = 0x7f080053;
-        public static final int icon = 0x7f08005c;
-        public static final int icon_group = 0x7f08005d;
-        public static final int info = 0x7f080060;
-        public static final int italic = 0x7f080062;
-        public static final int line1 = 0x7f080067;
-        public static final int line3 = 0x7f080068;
-        public static final int normal = 0x7f080081;
-        public static final int notification_background = 0x7f080082;
-        public static final int notification_main_column = 0x7f080084;
-        public static final int notification_main_column_container = 0x7f080085;
-        public static final int right_icon = 0x7f080095;
-        public static final int right_side = 0x7f080096;
-        public static final int tag_transition_group = 0x7f0800c5;
-        public static final int tag_unhandled_key_event_manager = 0x7f0800c6;
-        public static final int tag_unhandled_key_listeners = 0x7f0800c7;
-        public static final int text = 0x7f0800c8;
-        public static final int text2 = 0x7f0800c9;
-        public static final int time = 0x7f0800d1;
-        public static final int title = 0x7f0800d2;
+        public static final int icon = 0x7f08005d;
+        public static final int icon_group = 0x7f08005e;
+        public static final int info = 0x7f080061;
+        public static final int italic = 0x7f080063;
+        public static final int line1 = 0x7f080068;
+        public static final int line3 = 0x7f080069;
+        public static final int normal = 0x7f080082;
+        public static final int notification_background = 0x7f080083;
+        public static final int notification_main_column = 0x7f080085;
+        public static final int notification_main_column_container = 0x7f080086;
+        public static final int right_icon = 0x7f080097;
+        public static final int right_side = 0x7f080098;
+        public static final int tag_transition_group = 0x7f0800c7;
+        public static final int tag_unhandled_key_event_manager = 0x7f0800c8;
+        public static final int tag_unhandled_key_listeners = 0x7f0800c9;
+        public static final int text = 0x7f0800ca;
+        public static final int text2 = 0x7f0800cb;
+        public static final int time = 0x7f0800d3;
+        public static final int title = 0x7f0800d4;
     }
     public static final class integer {
         private integer() {}
@@ -115,17 +115,17 @@ public final class R {
     public static final class layout {
         private layout() {}
 
-        public static final int notification_action = 0x7f0b0030;
-        public static final int notification_action_tombstone = 0x7f0b0031;
-        public static final int notification_template_custom_big = 0x7f0b0038;
-        public static final int notification_template_icon_group = 0x7f0b0039;
-        public static final int notification_template_part_chronometer = 0x7f0b003d;
-        public static final int notification_template_part_time = 0x7f0b003e;
+        public static final int notification_action = 0x7f0b0031;
+        public static final int notification_action_tombstone = 0x7f0b0032;
+        public static final int notification_template_custom_big = 0x7f0b0039;
+        public static final int notification_template_icon_group = 0x7f0b003a;
+        public static final int notification_template_part_chronometer = 0x7f0b003e;
+        public static final int notification_template_part_time = 0x7f0b003f;
     }
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0e0045;
+        public static final int status_bar_notification_info_overflow = 0x7f0e0046;
     }
     public static final class style {
         private style() {}
